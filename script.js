c3288143b9cdaa4f47c42083dc121c43a1c9399a
Josh S <aaabccc87@protonmail.com>
2022-06-02 16:32:29 -0400

    Makes use of classes and modules

--- script.js
+++ script.js
@@ -100,168 +100,182 @@ const Library = (function (){
         }
     
         if (comparator !== undefined){
-            myLibrary.sort(comparator);
+            _myLibrary.sort(comparator);
         }
     
-        return myLibrary;
+        return getBooks();
+    }
+
+    return {
+        getBooks, 
+        getLastBookAdded, 
+        addBook, 
+        addBookFromInfo, 
+        addBooks, 
+        removeBook, 
+        sortLibrary
     }
 
 })();
 
-//Functionality in Library module
-// function sortBooks (byField){
-//     let comparator = undefined;
-//     if (byField === "title"){
-//         comparator = Book.compareTitles;
-//     }
-//     else if (byField === "author"){
-//         comparator = Book.compareAuthor;
-//     }
+const libraryController = ( function (){
+    const _openAddBookModalButton = document.querySelector("#open-add-book-modal");
+    const _cancelAddBookModalButton = document.querySelector("#cancel-add-book-button");
+    const _addBookForm = document.querySelector("#add-book-form");
+    const _addBookModal = document.querySelector("#add-book-modal");
+    const _bookTitleInput = document.querySelector("#book-title");
+    const _bookAuthorInput = document.querySelector("#book-author");
+    const _bookPagesInput = document.querySelector("#book-pages");
+    const _bookReadInput = document.querySelector("#book-read");
+    const _bookList = document.querySelector("#book-list");
+    const _bookRowTemplate = document.querySelector("#book-row");
+    const _titleHeader = document.querySelector("thead .title p");
+    const _authorHeader = document.querySelector("thead .author p");
 
-//     if (comparator !== undefined){
-//         myLibrary.sort(comparator);
-//     }
-
-//     return myLibrary;
-// }
-
-//Functionality to add book in Library module
-//Module does not display new book
-function addBookToLibrary(){
-    let title = bookTitleInput.value;
-    let author = bookAuthorInput.value;
-    let numPages = bookPagesInput.value;
-    let read = bookReadInput.checked;
-
-    let newBook = new Book(title, author, numPages, read);
-
-    myLibrary.push(newBook);
-    displayAddedBook(newBook, myLibrary.length - 1, myLibrary);
-}
+    let myLibrary = [ new Book("Lord of the Rings", "J.R.R. Tolkien", 323, true),
+                      new Book("Game of Thrones", "George R.R. Martin", 887, false),
+                      new Book("Harry Potter", "J.K. Rowling", 411, true),
+                      new Book("Moby Dick", "Herman Melville", 100, false)
+                    ];
 
 
-let myLibrary = [];
+    Library.addBooks(myLibrary);
 
-myLibrary.push(new Book("Lord of the Rings", "J.R.R. Tolkien", 323, true));
-myLibrary.push(new Book("Game of Thrones", "George R.R. Martin", 887, false));
-myLibrary.push(new Book("Harry Potter", "J.K. Rowling", 411, true));
-myLibrary.push(new Book("Moby Dick", "Herman Melville", 100, false));
+    _displayBooks();
 
-displayBooks();
+    _addBookForm.addEventListener("submit", (e) => {
+        e.preventDefault();
+        addBookToLibrary();
+    
+        //Form doesn't reset after call to preventDefault
+        _addBookForm.reset();
+    
+        _addBookModal.classList.toggle("open");
+    });
 
+    _titleHeader.addEventListener("click", () => {
+        Library.sortLibrary("title");
+        _displayBooks();
+    });
+    
+    _authorHeader.addEventListener("click", () => {
+        Library.sortLibrary("author");
+        _displayBooks();
+    });
 
+    _openAddBookModalButton.addEventListener("click", () => {
+        _addBookModal.classList.toggle("open");
+    });
 
+    _addBookModal.addEventListener("click", (e) => {
+        //Only want the add book dialog to disappear if user clicked out of it.
+        if (e.target !== _addBookModal){
+            return;
+        }
+        _addBookModal.classList.toggle("open");
+    
+    });
 
-//Functionality added to remove book in Library module
-//does not update display
-function removeBook(row){
-    let index = Number(row.getAttribute("data-index"));
-
-    myLibrary.splice(index, 1);
-
-    displayBooks();
+    _cancelAddBookModalButton.addEventListener("click", () =>{
+        _addBookModal.classList.toggle("open");
+    });
+    
+    
+    
     
 
-}
-
-
-
-const openAddBookModalButton = document.querySelector("#open-add-book-modal");
-const cancelAddBookModalButton = document.querySelector("#cancel-add-book-button");
-const addBookForm = document.querySelector("#add-book-form");
-const addBookModal = document.querySelector("#add-book-modal");
-const bookTitleInput = document.querySelector("#book-title");
-const bookAuthorInput = document.querySelector("#book-author");
-const bookPagesInput = document.querySelector("#book-pages");
-const bookReadInput = document.querySelector("#book-read");
-const bookList = document.querySelector("#book-list");
-const bookRowTemplate = document.querySelector("#book-row");
-const titleHeader = document.querySelector("thead .title p");
-const authorHeader = document.querySelector("thead .author p");
-
-titleHeader.addEventListener("click", () => {
-    sortBooks("title");
-    displayBooks();
-});
-
-authorHeader.addEventListener("click", () => {
-    sortBooks("author");
-    displayBooks();
-});
-openAddBookModalButton.addEventListener("click", () => {
-    addBookModal.classList.toggle("open");
-});
-
-addBookModal.addEventListener("click", (e) => {
-    //Only want the add book dialog to disappear if user clicked out of it.
-    if (e.target !== addBookModal){
-        return;
+    function addBookToLibrary(){
+        let title = _bookTitleInput.value;
+        let author = _bookAuthorInput.value;
+        let numPages = _bookPagesInput.value;
+        let read = _bookReadInput.checked;
+    
+        let newBook = new Book(title, author, numPages, read);
+    
+        Library.addBook(newBook);
+        displayAddedBook(newBook, myLibrary.length - 1, myLibrary);
     }
-    addBookModal.classList.toggle("open");
 
-});
+    function _clearBookList(){
+        const rows = _bookList.querySelectorAll("tr");
+    
+        rows.forEach( item => {
+            _bookList.removeChild(item);
+        });
+    }
 
-cancelAddBookModalButton.addEventListener("click", () =>{
-    addBookModal.classList.toggle("open");
-});
-
-addBookForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    addBookToLibrary();
-
-    //Form doesn't reset after call to preventDefault
-    addBookForm.reset();
-
-    addBookModal.classList.toggle("open");
-});
-
-
-
-
-function displayBooks(){
-    // Make sure list is clear before adding more so it doesn't pile on duplicate listings
-    clearBookList();
-
-    myLibrary.forEach( (book, i, arr) => {
-        let newRow = createBookRow(book, i, arr);
-        bookList.appendChild(newRow);
-    });
-}
-
-function displayAddedBook(newBook, i, arr){
-    let newRow = createBookRow(newBook, i, arr);
-    bookList.appendChild(newRow);
-}
-function createBookRow(book, i, arr){
-    const bookRow = bookRowTemplate.content.cloneNode(true);
-    const row = bookRow.querySelector("tr");
-    const cell = bookRow.querySelectorAll("td");
-
-    row.setAttribute("data-index", i);
-    cell[0].textContent = book.title;
-    cell[1].textContent = book.author;
-    cell[2].textContent = book.numPages ? book.numPages : '-';
-    cell[3].textContent = book.read ? "✔" : "-";
-
-    cell[3].addEventListener("click", (e) => {
-        book.toggleRead();
+    function _createBookRow(book, i, arr){
+        const bookRow = _bookRowTemplate.content.cloneNode(true);
+        const row = bookRow.querySelector("tr");
+        const cell = bookRow.querySelectorAll("td");
+    
+        row.setAttribute("data-index", i);
+        cell[0].textContent = book.title;
+        cell[1].textContent = book.author;
+        cell[2].textContent = book.numPages ? book.numPages : '-';
         cell[3].textContent = book.read ? "✔" : "-";
-    });
+    
+        cell[3].addEventListener("click", (e) => {
+            book.toggleRead();
+            cell[3].textContent = book.read ? "✔" : "-";
+        });
+    
+        cell[4].addEventListener("click", (e) => { 
+            e.stopPropagation();
+            removeBook(row);
+        });
+    
+        return bookRow;
+    }
 
-    cell[4].addEventListener("click", (e) => { 
-        e.stopPropagation();
-        removeBook(row);
-    });
+    function _displayBooks(){
+        // Make sure list is clear before adding more so it doesn't pile on duplicate listings
+        _clearBookList();
+    
 
-    return bookRow;
-}
+        Library.getBooks().forEach( (book, i, arr) => {
+            let newRow = _createBookRow(book, i, arr);
+            _bookList.appendChild(newRow);
+        });
+    }
+
+    function displayAddedBook(newBook, i, arr){
+        let newRow = _createBookRow(newBook, i, arr);
+        _bookList.appendChild(newRow);
+    }
+
+    function removeBook(row){
+        let index = Number(row.getAttribute("data-index"));
+    
+        Library.removeBook(index);
+    
+        _displayBooks();
+        
+    
+    }
+    
+    
+    
+})();
 
 
 
-function clearBookList(){
-    const rows = bookList.querySelectorAll("tr");
 
-    rows.forEach( item => {
-        bookList.removeChild(item);
-    })
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

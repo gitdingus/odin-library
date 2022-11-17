abb9dd0128cb20841e172122c1999396cb08a427
Josh S <aaabccc87@protonmail.com>
2022-11-17 16:15:03 -0500

    Add support for css modules

--- webpack-dev.config.js
+++ webpack-dev.config.js
@@ -12,7 +12,20 @@ module.exports = {
     rules: [
       {
         test: /\.css$/,
+        exclude: /\.module\.css$/,
         use: [ 'style-loader', 'css-loader' ],
+      },
+      {
+        test: /\.module\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+            },
+          },
+        ],
       },
       {
         test: /\.png$/,

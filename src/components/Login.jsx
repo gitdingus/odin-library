10b1ff361ce4f113067e0f928d56908385b5f899
Josh S <aaabccc87@protonmail.com>
2022-11-17 16:13:52 -0500

    Add functionality to pass values for logging, creating new user. Simple display for logged in user

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,23 +1,121 @@
 import React, { useState } from 'react';
+import styles from './styles/login.module.css';
 
-export default function Login({user}) {
-  const [mode, setMode] = useState('login');
+export default function Login({user, login, createUser}) {
+  const [mode, setMode] = useState('display');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
 
-  const loginUser = () => {
-    console.log('login user');
+  const loginClicked = (e) => {
+    e.preventDefault();
+
+    login({
+      username,
+      password,
+    });
+    
+    setMode('display');
   };
+
+  const createUserClicked = (e) => {
+    e.preventDefault();
+
+    if (password === confirmPassword) {
+      createUser({
+        username,
+        password,
+      });
+
+      setMode('display');
+    }
+  };
+
+  const cancelForm = () => {
+    setUsername('');
+    setPassword('');
+    setConfirmPassword('');
+    setMode('display');
+  }
+
+  if (mode === 'display') {
+    if (user === null) {
+      return (
+        <div className={styles.buttons}>
+          <button type="button" onClick={() => { setMode('login'); }}>Login</button>
+          <button type="button" onClick={() => { setMode('create'); }}>Create User</button>
+        </div>
+      );
+    }
+
+    return (
+      <div>
+        <p>{user.username} logged in</p>
+      </div>
+    )
+  }
 
   if (mode === 'login') {
     return (
-      <div>
-        <form noValidate>
-          <label htmlFor="username">User: </label>
-          <input type="text" id="username" />
-          <label htmlFor="password">Password: </label>
-          <input type="password" id="password" />
-          <button type="submit" onClick={loginUser}>Login</button>
+      <div className="modal open">
+        <form>
+          <h1>Login</h1>
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)} 
+          />
+          <div className="buttons">
+            <button type="button" onClick={cancelForm}>Cancel</button>
+            <button type="submit" onClick={loginClicked}>Log in</button>
+          </div>
         </form>
       </div>
     )
   }
+
+  if (mode === 'create') {
+    return (
+      <div className="modal open">
+        <form>
+          <h1>Login</h1>
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <label htmlFor="confirm-password">Confirm Password</label>
+          <input
+            type="password"
+            id="confirm-password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+          <div className="buttons">
+            <button type="button" onClick={cancelForm}>Cancel</button>
+            <button type="submit" onClick={createUserClicked}>Log in</button>
+          </div>
+        </form>
+      </div>
+    )
+  }
+
 }
